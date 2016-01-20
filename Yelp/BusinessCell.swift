72996a653170d1b62ad402953e1ae2a6a8393466
iTwicKz <twickes32@ufl.edu>
2016-01-19 22:10:42 -0500

    Auto-layout tutorial completed

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -36,6 +36,13 @@ class BusinessCell: UITableViewCell {
         // Initialization code
         thumbImageView.layer.cornerRadius = 9
         thumbImageView.clipsToBounds = true
+        
+        
+//        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
